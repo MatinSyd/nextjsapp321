70c0ee56bd9bca6a22e0967f6df4ec9fd511ca46
MatinSyd <93215557+MatinSyd@users.noreply.github.com>
2024-02-07 12:12:20 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -17,7 +17,7 @@ const Navbar = () => {
     return (
         <>
       <nav className="navbar navbar-expand-lg navbar-dark bg-success fixed-top">
-  <Link  href="/"><a className="navbar-brand">BLOGGER</a></Link>
+  <Link  href="/"><a className="navbar-brand">blogger</a></Link>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
